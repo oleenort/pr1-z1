d5e4b31803b15d39c787ecda024d81f26a7e0edd
oleenort <yrfy111@gmail.com>
2020-09-11 14:08:22 +0300

    черт блин

--- praktika2-ypr3/praktika2-ypr3/Program.cs
+++ praktika2-ypr3/praktika2-ypr3/Program.cs
@@ -6,7 +6,10 @@ namespace praktika2_ypr3
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      string s = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = s.Split(" ");
+      Array.Sort(sa);
+      s = string.Join(" ", sa); Console.WriteLine(s);
     }
   }
 }
