27adf0e3acf313d2947ed39b9857c4a14e087fda
oleenort <yrfy111@gmail.com>
2020-09-11 14:46:37 +0300

    очень вкусно спасибо

---
+++ pr02/praktika2-ypr4/praktika2-ypr4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace praktika2_ypr4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Int16 i16 = 1; Int32 i32 = 1; double db = 1;
+      i16 = 132;
+      i16 = db;
+      i32 = i16;
+      i32 = db;
+      db = 116; db = i32;
+
+
+    }
+  }
+}
